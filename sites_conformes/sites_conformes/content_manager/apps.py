e39c11e28126eddb9d821f30221927b8953d8060
Fabien Le Frapper <contact@fabienlefrapper.me>
2026-05-13 13:10:23 +0200

    Add sites_conformes package for v3.1.0

---
+++ sites_conformes/sites_conformes/content_manager/apps.py
@@ -0,0 +1,7 @@
+from django.apps import AppConfig
+
+
+class ContentManagerConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "sites_conformes.content_manager"
+    label = "sites_conformes_content_manager"
