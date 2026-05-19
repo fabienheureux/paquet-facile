500a2d07fc3b6f3b7826a6eb0d4ad7bd744c8574
Fabien Le Frapper <contact@fabienlefrapper.me>
2026-05-19 12:39:56 +0200

    Add sites_conformes package for v3.2.0

---
+++ sites_conformes/sites_conformes/config/settings_test.py
@@ -0,0 +1,12 @@
+from config.settings import *  # NOSONAR # noqa: F401,F403
+
+# Enable db_storage for testing its functionality
+SF_USE_DB_STORAGE = True
+if "sites_conformes.db_storage" not in INSTALLED_APPS:  # noqa: F405
+    INSTALLED_APPS.insert(-1, "sites_conformes.db_storage")  # noqa: F405
+
+WHITENOISE_AUTOREFRESH = True
+WHITENOISE_MANIFEST_STRICT = False
+
+FORCE_SCRIPT_NAME = ""
+WAGTAILADMIN_BASE_URL = "http://localhost"
